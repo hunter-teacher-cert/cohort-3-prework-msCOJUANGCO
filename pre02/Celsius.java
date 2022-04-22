25b10d81c38d64f7340198c757bb0111e9110473
Nicole Cojuangco <54820325+msCOJUANGCO@users.noreply.github.com>
2022-04-22 20:17:06 +0000

    complete & check celsius exercise

--- pre02/Celsius.java
+++ pre02/Celsius.java
@@ -1,26 +1,40 @@
 import java.util.Scanner;
 
+
+
 /**
+Write a program that converts a temperature from Celsius to Fahrenheit. It should (1) prompt the user for input, (2) read a double value from the keyboard, (3) calculate the result, and (4) format the output to one decimal place. For example, it should display "24.0 C = 75.2 F".
+Here is the formula. Be careful not to use integer division!
+
+see equation in textbook
+____________________________
+
  * Converts centimeters to feet and inches.
  */
+
+
 public class Celsius {
 
+    
     public static void main(String[] args) {
-        double cm;
-        int feet, inches, remainder;
-        final double CM_PER_INCH = 2.54;
-        final int IN_PER_FOOT = 12;
-        Scanner in = new Scanner(System.in);
+    double cel;
+    double far;
+    //int feet, inches, remainder;
+    //final double CM_PER_INCH = 2.54;
+    //final int IN_PER_FOOT = 12;
+    Scanner in = new Scanner(System.in);
 
-        // prompt the user and get the value
-        System.out.print("Exactly how many cm? ");
-        cm = in.nextDouble();
+// A. prompt the user for input
+System.out.print("Temperature in Celsius? ");
 
-        // convert and output the result
-        inches = (int) (cm / CM_PER_INCH);
-        feet = inches / IN_PER_FOOT;
-        remainder = inches % IN_PER_FOOT;
-        System.out.printf("%.2f cm = %d ft, %d in\n",
-                          cm, feet, remainder);
+// B. read a double value from the keyboard
+cel = in.nextDouble();
+
+// C. calculate the result
+far = (double) ((cel*(9.0/5.0)) + 32);
+
+// D. format the output to one decimal place. For example, it should display "24.0 C = 75.2 F".
+System.out.printf(cel + " C = %.1f F", far);
+
     }
 }
