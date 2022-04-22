065d9ee181525a0cbeb8fcfbee5d8168877d6055
Nicole Cojuangco <54820325+msCOJUANGCO@users.noreply.github.com>
2022-04-22 20:19:56 +0000

    deleted unnecessary line

--- pre02/Celsius.java
+++ pre02/Celsius.java
@@ -1,21 +1,17 @@
 import java.util.Scanner;
 
-
-
 /**
 Write a program that converts a temperature from Celsius to Fahrenheit. It should (1) prompt the user for input, (2) read a double value from the keyboard, (3) calculate the result, and (4) format the output to one decimal place. For example, it should display "24.0 C = 75.2 F".
 Here is the formula. Be careful not to use integer division!
-
-see equation in textbook
 ____________________________
 
- * Converts centimeters to feet and inches.
- */
+see equation in textbook
 
+ */
 
 public class Celsius {
 
-    
+
     public static void main(String[] args) {
     double cel;
     double far;
