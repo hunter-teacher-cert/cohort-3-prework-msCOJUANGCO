27430985f41c15b8e4e145dd2a4514278a498a0a
Nicole Cojuangco <54820325+msCOJUANGCO@users.noreply.github.com>
2022-04-26 18:57:21 +0000

    redoing exercise 4

--- pre06/loops.java
+++ pre06/loops.java
@@ -4,11 +4,11 @@ import java.util.Scanner;
 import java.lang.Math;
 
 /*************************
-A. CH7.9_Exercise1 (see pdf) 
+A. CH7.9_Eyercise1 (see pdf) 
 
-B. CH7.9_Exercise2 
-C. CH7.9_Exercise3
-D. CH7.9_Exercise4 
+B. CH7.9_Eyercise2 
+C. CH7.9_Eyercise3
+D. CH7.9_Eyercise4 
 ********************************/
 public class loops {
 
@@ -16,7 +16,7 @@ public class loops {
 public  static void main (String[]args) {
 
         double a; //target num (why over 2?)
-        double x; //first iteration/guess
+        double y; //first iteration/guess
     
 Scanner in = new Scanner(System.in);
 
@@ -25,54 +25,73 @@ System.out.println("Choose a positive whole number.");
     a = in.nextInt(); 
 System.out.printf("\nOk! Let's find the square root of %2f!\n", a);
 
-x = a/2.0;
-System.out.println(squareROOT(a, x));
+y = a/2.0;
+System.out.println(squareROOT(a, y));
 
+
+        double x; 
+        int n;
+System.out.println(power(2.0, 0));
+
+        int z;
+System.out.println(factorial(9));
     }
     
 
-//B.    
-public  static double squareROOT (double a, double x) {
+//B.*************************************    
+public  static double squareROOT (double a, double y) {
 
             double i;  //difference        
             double sqrt;
-//verify expression
-    //System.out.printf("\nFirst approximation is: %4f.", sqrt);
+//verify eypression
+    //System.out.printf("\nFirst approyimation is: %4f.", sqrt);
 
-    sqrt = (x+(a/x))/2.0;
-    i = Math.abs(x - sqrt);
+    sqrt = (y+(a/y))/2.0;
+    i = Math.abs(y - sqrt);
 
     while (i >= 0.0001) {
-            sqrt = (x+(a/x))/2.0;
+            sqrt = (y+(a/y))/2.0;
             System.out.println(sqrt);
-            i = Math.abs(x - sqrt);
-            x = sqrt;
+            i = Math.abs(y - sqrt);
+            y = sqrt;
         }
     return sqrt;
     }
 
-}
     
-    /**process & print (use do/while sample from text) 
-boolean okay;
-    do {
-        System.out.print("Enter a number: ");
-    if (in.hasNextDouble()) {
-        okay = true;
-    } else {
-        okay = false;
-        String word = in.next();
-        System.err.println(word + " is not a number");
+//C.*************************************    
+public static double power (double x, int n ) {
+
+    int i=1;
+    
+    if (n == 0) {
+        return 1;
+    
+    } else
+        while (i <= (n-1) ) {
+            i=1;
+            x *= x;
+            i += i;
+            }
+    return x;
     }
-} while (!okay);
-double x = in.nextDouble();
 
+/**D.*************************************    
+public static int factorial (int z) {
+        int prod;
+    prod =1;
+    
+    if (z == 0) {
+        return 1;
+    
+    } else
 
+        for (z >= 1 ) {
+            prod = (z*(z-1));
+            z = z-1 ;
 
-
-   
-
-    }   
+            }
+    return prod;
+    }
+   */ 
 }
-
-*/
