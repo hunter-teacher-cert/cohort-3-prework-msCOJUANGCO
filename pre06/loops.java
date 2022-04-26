9b06f1100e1cb7a4d1f00de84e5375b4d1c935b4
Nicole Cojuangco <54820325+msCOJUANGCO@users.noreply.github.com>
2022-04-26 11:08:12 +0000

    ;

--- pre06/loops.java
+++ pre06/loops.java
@@ -16,43 +16,41 @@ public class loops {
 public  static void main (String[]args) {
 
         double a; //target num (why over 2?)
+        double guess1; //first guess
     
     Scanner in = new Scanner(System.in);
 
     //prompt user to choose target number 
     System.out.println("What whole number would you like to find the square root of?");
-    
     a = in.nextInt(); 
     
     //print target num & input 1st guess
     System.out.printf("\nOk! Let's find the square root of %2f! \nWhat do you think its square root might be?", a);
-    
-    System.out.println(squareROOT(a));
+    guess1 = in.nextInt(); 
+
+    System.out.println(squareROOT(a, guess1));
     }
 
     
 //B.    
-public  static double squareROOT (double a) {
+public  static double squareROOT (double a, double guess1) {
 
 Scanner in = new Scanner(System.in);
 
-            double guess1; //first guess
-            double sqrt; //output
-
-    guess1 = in.nextInt(); //first guess
+            double diff;            
+            double sqrt;
+            sqrt = (guess1+(a/guess1))/2.0; 
+            diff = (Math.abs(a - sqrt));
     
     //process & print first guess 
-        //sqrt = (guess1+(a/guess1))/2.0;
         //System.out.printf("\nFirst approximation is: %4f.", sqrt);
     
     //process & print 
         double i = a;
-        double a2 = 1;
-        double diff;
-    
-    while (Math.abs(a - a2) >= 0.0001) {
+        double a2 = sqrt;
+       
+    while (diff >= 0.0001) {
         i = sqrt = (guess1+(i/guess1))/2.0;
-        sqrt = a2;
         System.out.println(a2);
         //i = a2;
         }
