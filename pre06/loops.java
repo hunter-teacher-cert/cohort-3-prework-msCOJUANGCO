53c9f349f41df695a22e7d0f715a18e1a62750ad
Nicole Cojuangco <54820325+msCOJUANGCO@users.noreply.github.com>
2022-04-26 12:10:46 +0000

    modified loops

--- pre06/loops.java
+++ pre06/loops.java
@@ -16,45 +16,58 @@ public class loops {
 public  static void main (String[]args) {
 
         double a; //target num (why over 2?)
-        double guess1; //first guess
+        double x; //first guess
     
-    Scanner in = new Scanner(System.in);
-
-    //prompt user to choose target number 
-    System.out.println("What whole number would you like to find the square root of?");
-    a = in.nextInt(); 
-    
-    //print target num & input 1st guess
-    System.out.printf("\nOk! Let's find the square root of %2f! \nWhat do you think its square root might be?", a);
-    guess1 = in.nextInt(); 
-
-    System.out.println(squareROOT(a, guess1));
-    }
-
-    
-//B.    
-public  static double squareROOT (double a, double guess1) {
-
 Scanner in = new Scanner(System.in);
 
-            double diff;            
-            double sqrt;
-            sqrt = (guess1+(a/guess1))/2.0; 
-            diff = (Math.abs(a - sqrt));
+//prompt user to choose target number 
+System.out.println("Choose a positive whole number.");
+    a = in.nextInt(); 
+System.out.printf("\nOk! Let's find the square root of %2f!\n", a);
+ 
+System.out.println(squareROOT(a));
+
+    }
+}
     
-    //process & print first guess 
-        //System.out.printf("\nFirst approximation is: %4f.", sqrt);
+
+//B.    
+public  static double squareROOT (double a, double x) {
+
+            double i;    //difference        
+            double sqrt; //1st iteration
     
-    //process & print 
-        double i = a;
-        double a2 = sqrt;
-       
-    while (diff >= 0.0001) {
-        i = sqrt = (guess1+(i/guess1))/2.0;
-        System.out.println(a2);
+//verify expression
+    //System.out.printf("\nFirst approximation is: %4f.", sqrt);
+    
+//process & print (use do/while sample from text) 
+boolean okay;
+    do {
+        System.out.print("Enter a number: ");
+    if (in.hasNextDouble()) {
+        okay = true;
+    } else {
+        okay = false;
+        String word = in.next();
+        System.err.println(word + " is not a number");
+    }
+} while (!okay);
+double x = in.nextDouble();
+
+
+
+
+    x = a/2.0;
+    while (i >= 0.0001) {
+        System.out.println(sqrt);
+        sqrt = (guess1+(a/guess1))/2.0; 
+        System.out.println(guess1);
+        i = (Math.abs(guess1 - sqrt));
+
         //i = a2;
         }
-    return Math.abs(a - a2);
+    return i;
 
     }   
 }
+*/
