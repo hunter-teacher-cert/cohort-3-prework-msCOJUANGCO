7fdaffa536d0c73dd58dced20aa445f3734ae451
Nicole Cojuangco <54820325+msCOJUANGCO@users.noreply.github.com>
2022-05-02 00:32:00 +0000

    modified histogram

--- pre07/array.java
+++ pre07/array.java
@@ -2,12 +2,11 @@ import java.io.*;
 import java.util.*;
 import java.util.Scanner;
 import java.lang.Math;
-import java.lang.Random;
 
 
 /*************************
-A. powArray & historgram
-
+A. powArray 
+B. histogram
 ********************************/
 public class array {
 
@@ -18,7 +17,8 @@ public static void main(String[] args) {
     
     powArray (a,power);
     }
-    
+
+//A. powArray
 public static void powArray(double [] a,double power) {
 
     for (int i = 0; i < a.length; i++) {
@@ -26,45 +26,24 @@ public static void powArray(double [] a,double power) {
         result = a[i]*power;
         System.out.println(result);
         }
+
+
+    }
+
+//B. histogram
+public static void histogram(double [] a,double power) {
+
+    for (int i = 0; i < a.length; i++) {
+        double result;
+        result = a[i]*power;
+        System.out.println(result);
+        }
+
+
     }
 
 
 
-public static int histogram(int[] b, int low, int high) {
-                            int count = 0;
+
     
-    for (int i = 0; i < b.length; i++) {
-        if (b[i] >= low && a[i] < high) {
-            count++;
-
-            
-    int[] scores = randomArray(100);
-    int a = inRange(scores, 90, 100);
-    int b = inRange(scores, 80, 90);
-    int c = inRange(scores, 70, 80);
-    int d = inRange(scores, 60, 70);
-    int f = inRange(scores, 0, 60);
-    int[] counts = new int[100];
-            
-    for (int i = 0; i < counts.length; i++) {
-    counts[i] = inRange(scores, i, i + 1);
-
-
-
-    return count;
-
-
-    *******
-
-for (int i = 0; i < values.length; i++) {
-    int value = values[i];
-    System.out.println(value);
-}
-We could rewrite the loop like this:
-
-for (int value : values) {
-    System.out.println(value);
-}
-
-
 }
--- pre07/exercises.java
+++ pre07/exercises.java
@@ -1,25 +1,70 @@
 import java.util.Arrays;
-import java.io.*;
-import java.util.*;
-import java.util.Scanner;
-import java.lang.Math;
+import java.util.Random;
+import java.io.PrintStream;
 
 public class exercises {
 
+    
 public static void main(String[] args) {
-        double [] a = {2,3,4};
-        double power;
-        power = 3.0;
-    
-    powArray (a,power);
-    }
-    
-public static void powArray(double [] a,double power) {
 
+    //PRINTS RANDOMIZED NUMBER LIST*****************
+        
+        int scores = 3; // # of values; can change
+        int[] counts = new int[3];// # of counts; can change
+        int[] array = randomArray(scores); //create
+
+System.out.printf("Our array has  %s values.", scores); 
+System.out.println("\nThis is our array:");
+printArray(array); //print
+
+    for (int score : array) {
+        counts[score]++;
+printArray(counts);
+            }
+}
+//public static int historgram(int[] array){
+    
+
+
+
+/*/TRAVERSE + COUNT ARRAY*************************
+public static int inRange(int[] a, int low, int high) {
+    int count = 0;
     for (int i = 0; i < a.length; i++) {
-        double result;
-        result = a[i]*power;
-        System.out.println(result);
+        if (a[i] >= low && a[i] < high) {
+            count++;
         }
     }
+    return count;
 }
+*/
+
+
+  //CREATES RANDOM ARRAY*************************
+public static int[] randomArray(int size) {
+    Random random = new Random();
+    int[] a = new int[size];
+    for (int i = 0; i < a.length; i++) {
+        a[i] = random.nextInt(100);
+        }
+      
+    return a;
+      }
+  
+//PRINTS ARRAYS*************************
+public static void printArray(int[] a) {
+    System.out.print("{" + a[0]);
+    for (int i = 1; i < a.length; i++) {
+        System.out.print(", " + a[i]);
+    }
+    System.out.println("}");
+}
+
+
+
+    
+}
+
+
+
+
